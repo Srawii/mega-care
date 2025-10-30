4ff21dfcd4208e0315d704c7e024be9e77d397e5
Srawii <160399749+Srawii@users.noreply.github.com>
2025-10-30 16:10:35 +0700

    Update header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <script type="text/javascript" src="https://app.adtechthai.com/js/1.0/adtech-tracker.js" ></script>
+        <script> 
+          if( document.readyState === "complete" ||document.readyState === "interactive" ) { 
+            adTechTrackerInit("rqhY3MysIngzm4Sqvno2x4C3qqUiFxqL");
+          } else {
+            document.addEventListener("DOMContentLoaded", function(event) { 
+              adTechTrackerInit("rqhY3MysIngzm4Sqvno2x4C3qqUiFxqL"); 
+            }); 
+          }
+        </script>
+        <!-- Meta Pixel Code -->
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1460124498614851');
+          fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1460124498614851&ev=PageView&noscript=1"/></noscript>
+        <!-- End Meta Pixel Code -->
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
